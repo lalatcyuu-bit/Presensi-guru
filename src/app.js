31274ddd4388a90e9799841cf56077546a8eb129
naufalaziz937 <naufalaziz937@gmail.com>
2026-02-01 20:31:18 +0700

    feat: enhance guru management with mapel validation and queries

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://bgdr3s45-3000.asse.devtunnels.ms',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 app.use(express.json());
 
@@ -8,6 +16,9 @@ app.use('/auth', require('./routes/auth.routes'));
 app.use('/users', require('./routes/user.routes'));
 app.use('/guru', require('./routes/guru.routes'));
 app.use('/mapel', require('./routes/mapel.routes'));
+
+
+
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
--- src/routes/guru.routes.js
+++ src/routes/guru.routes.js
@@ -6,5 +6,8 @@ const role = require('../middleware/role.middleware');
 const guruController = require('../controllers/guru.controller');
 
 router.post('/', auth, role.onlyAdmin, guruController.createGuru);
-
+router.get('/', auth, role.onlyAdmin, guruController.getGuru);
+router.get('/search', auth, role.onlyAdmin, guruController.getGuruByMapel);
+router.put('/:id', auth, role.onlyAdmin, guruController.updateGuru);
+router.delete('/:id', auth, role.onlyAdmin, guruController.deleteGuru);
 module.exports = router;
