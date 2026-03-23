80f3ce0e25420d70361b3c3a6cba6e6b95bd64d3
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-03-23 16:23:28 +0700

    feat: Remove admin role restriction from kalender retrieval route

--- src/routes/kalender.routes.js
+++ src/routes/kalender.routes.js
@@ -8,7 +8,7 @@ const kalender = require('../controllers/kalender.controller');
 // PENTING: /check harus sebelum /:id agar tidak di-intercept sebagai id
 router.get('/check', auth, kalender.checkLiburHariIni);
 
-router.get('/', auth, role.onlyAdmin, kalender.getKalender);
+router.get('/', auth, kalender.getKalender);
 router.post('/', auth, role.onlyAdmin, kalender.createKalender);
 router.put('/:id', auth, role.onlyAdmin, kalender.updateKalender);
 router.delete('/:id', auth, role.onlyAdmin, kalender.deleteKalender);
